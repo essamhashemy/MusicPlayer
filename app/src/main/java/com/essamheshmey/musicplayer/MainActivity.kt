4cd99dc4bb664c1b76c4f54e14a74079fcfd299c
essam <essamheshmey>
2023-03-06 23:39:43 +0200

    null commint

--- app/src/main/java/com/essamheshmey/musicplayer/MainActivity.kt
+++ app/src/main/java/com/essamheshmey/musicplayer/MainActivity.kt
@@ -4,18 +4,24 @@ import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageButton
+import android.widget.SeekBar
 
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var runnable: Runnable
+private var handler=Handler()
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val butplay:ImageButton=findViewById(R.id.but_play)
-val mediaplayer:MediaPlayer=MediaPlayer.create(this,R.raw.a)
+        val seekbar:SeekBar=findViewById(R.id.seekbar)
 
+val mediaplayer:MediaPlayer=MediaPlayer.create(this,R.raw.a)
+        seekbar.progress=0
+        seekbar.max=mediaplayer.duration
         butplay.setOnClickListener(){
             if (!mediaplayer.isPlaying){
                 mediaplayer.start()
@@ -25,5 +31,19 @@ val mediaplayer:MediaPlayer=MediaPlayer.create(this,R.raw.a)
                 butplay.setImageResource(R.drawable.i_play)
             }
         }
+seekbar.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        if (p2){
+            mediaplayer.seekTo(p1)
+        }
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(p0: SeekBar?) {
+
+    }
+})
     }
 }
