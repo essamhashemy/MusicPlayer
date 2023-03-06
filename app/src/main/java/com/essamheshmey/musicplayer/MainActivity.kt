118706fb60c8d3bd4afe39fc356cbf5b89bb220d
essam <essamheshmey>
2023-03-06 21:39:31 +0200

    اضافت ملف سوط بسيط عمل بعض التعتيلات علي ملف main activ عمل لوجك بسيط لتجربت تشغيل النلفات الصوطية

--- app/src/main/java/com/essamheshmey/musicplayer/MainActivity.kt
+++ app/src/main/java/com/essamheshmey/musicplayer/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.essamheshmey.musicplayer
 
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val butplay:ImageButton=findViewById(R.id.but_play)
+val mediaplayer:MediaPlayer=MediaPlayer.create(this,R.raw.a)
+
+        butplay.setOnClickListener(){
+            if (!mediaplayer.isPlaying){
+                mediaplayer.start()
+                butplay.setImageResource(R.drawable.i_pause)
+            }else{
+                mediaplayer.pause()
+                butplay.setImageResource(R.drawable.i_play)
+            }
+        }
     }
 }
